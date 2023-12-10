678836e3ebc6d592aac86b0806e4369df2ac3c7e
pikol93 <pikol93@wp.pl>
2023-12-10 22:34:59 +0100

    Cargo format

--- task3/src/part2.rs
+++ task3/src/part2.rs
@@ -14,25 +14,26 @@ const SYMBOL_NEIGHBOUR_OFFSETS: [(i32, i32); 8] = [
 pub fn part2(input_text: &str) {
     let engine_schematic = read_input(input_text);
 
-    let sum = engine_schematic.symbols
+    let sum = engine_schematic
+        .symbols
         .iter()
         .filter(|symbol| symbol.character == '*')
         .filter_map(|symbol| {
             let mut symbol_neighbours = SYMBOL_NEIGHBOUR_OFFSETS.clone();
-            symbol_neighbours.iter_mut()
+            symbol_neighbours
+                .iter_mut()
                 .for_each(|(offset_x, offset_y)| {
                     *offset_x += symbol.char_index as i32;
                     *offset_y += symbol.line as i32;
                 });
 
-            let mut part_numbers = engine_schematic.part_numbers
-                .iter()
-                .filter(|part_number| {
-                    symbol_neighbours.iter()
-                        .filter(|(_, point_y)| part_number.line == *point_y as usize)
-                        .filter(|(point_x, _)| part_number.start_char_index <= *point_x as usize)
-                        .any(|(point_x, _)| part_number.end_char_index >= *point_x as usize)
-                });
+            let mut part_numbers = engine_schematic.part_numbers.iter().filter(|part_number| {
+                symbol_neighbours
+                    .iter()
+                    .filter(|(_, point_y)| part_number.line == *point_y as usize)
+                    .filter(|(point_x, _)| part_number.start_char_index <= *point_x as usize)
+                    .any(|(point_x, _)| part_number.end_char_index >= *point_x as usize)
+            });
 
             let Some(first_part_number) = part_numbers.next() else {
                 return None;
@@ -47,9 +48,7 @@ pub fn part2(input_text: &str) {
                 return None;
             }
 
-            Some(
-                first_part_number.value * second_part_number.value
-            )
+            Some(first_part_number.value * second_part_number.value)
         })
         .sum::<u32>();
 
