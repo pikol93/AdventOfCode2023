1f683b335198f2670380385aad6f8073f058909c
pikol93 <pikol93@wp.pl>
2023-12-11 18:26:21 +0100

    TASK-4 Implemented part 2

--- task4/src/common.rs
+++ task4/src/common.rs
@@ -2,8 +2,10 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 pub struct CardData {
+    pub card_number: u32,
     pub winning_numbers: Vec<u32>,
     pub owned_numbers: Vec<u32>,
+    pub win_count: usize,
 }
 
 pub fn read_input(input_text: &str) -> Vec<CardData> {
@@ -11,9 +13,11 @@ pub fn read_input(input_text: &str) -> Vec<CardData> {
 }
 
 fn read_line(line: &str) -> Option<CardData> {
-    let Some((_, right)) = line.split_once(':') else {
+    let Some((left, right)) = line.split_once(':') else {
         return None;
     };
+
+    let card_number = read_card_number(left).unwrap();
 
     let right = right.trim();
 
@@ -24,10 +28,29 @@ fn read_line(line: &str) -> Option<CardData> {
     let winning_numbers = read_numbers(winning_numbers_string);
     let owned_numbers = read_numbers(owned_numbers_string);
 
+    let win_count = owned_numbers
+        .iter()
+        .filter(|number| winning_numbers.contains(number))
+        .count();
+
     Some(CardData {
+        card_number,
         winning_numbers,
         owned_numbers,
+        win_count,
     })
+}
+
+fn read_card_number(text: &str) -> Option<u32> {
+    let Some((_, right)) = text.rsplit_once(' ') else {
+        return None;
+    };
+
+    let Ok(number) = u32::from_str(right) else {
+        return None;
+    };
+
+    Some(number)
 }
 
 fn read_numbers(text: &str) -> Vec<u32> {
--- task4/src/part2.rs
+++ task4/src/part2.rs
@@ -1,1 +1,18 @@
-pub fn part2(input_text: &str) {}
+use crate::common::read_input;
+
+pub fn part2(input_text: &str) {
+    let cards_data = read_input(input_text);
+    let mut card_count = vec![1; cards_data.len()];
+
+    for index in 0..cards_data.len() {
+        let count = *card_count.get(index).unwrap();
+        let card_data = cards_data.get(index).unwrap();
+        for i in 0..card_data.win_count {
+            let a = card_count.get_mut(index + i + 1).unwrap();
+            *a += count;
+        }
+    }
+
+    let sum = card_count.iter().sum::<u32>();
+    println!("Part 2 sum: {}", sum);
+}
